c61c46f7282957fd3de66881c48d6fa98a1716e1
sksh70 <22228680+sksh70@users.noreply.github.com>
2021-03-14 20:55:46 +0800

    Update Lab4Practice1.cpp

--- Lab/Lab4Practice1.cpp
+++ Lab/Lab4Practice1.cpp
@@ -6,16 +6,16 @@ Write a program to display the numbers 1 through 10 and their squares as demonst
 
 Number		Number per squared
 ========	==================
-1					1
-2					4
-3					9
-4					16
-5					25
-6					36
-7					49
-8					64
-9					81
-10					100
+1			1
+2			4
+3			9
+4			16
+5			25
+6			36
+7			49
+8			64
+9			81
+10			100
 */
 #include <iostream>
 #include <iomanip>
